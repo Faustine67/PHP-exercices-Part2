6d661bb616f805e69320fe7cb61ed3091475ca83
Faustine67 <michelfaustine@gmail.com>
2022-11-25 10:37:22 +0100

    exo 13 et 14

--- exo14.php
+++ exo14.php
@@ -1,13 +1,13 @@
 <h1> Exercice 14 </h1>
 
 <p>
-Créer une classe Voiture possédant 2 propriétés (marque et modèle) ainsi qu’une classe VoitureElec 
-qui hérite (extends) de la classe Voiture et qui a une propriété supplémentaire (autonomie).
+    Créer une classe Voiture possédant 2 propriétés (marque et modèle) ainsi qu’une classe VoitureElec
+    qui hérite (extends) de la classe Voiture et qui a une propriété supplémentaire (autonomie).
 </p>
 
 <p>
-nstancier une voiture « classique » et une voiture « électrique » ayant les caractéristiques 
-suivantes :
+    nstancier une voiture « classique » et une voiture « électrique » ayant les caractéristiques
+    suivantes :
 </p>
 
 Peugeot 408 : $v1 = new Voiture("Peugeot","408"); <br>
@@ -19,18 +19,55 @@ echo $ve1->getInfos()."<br>";
 <h2> Resultats </h2>
 
 <?php
-    Class Voiture{
-        private$_marque;
-        private$_modele;
- 
-    public function __construct($marque,$modele){
-            $this->_marque=$marque;
-            $this->_modele=$modele;
-        }
-    public function getInfos(){
-        return $this->_marque;
-        return $this->_modele; 
-}
+class Voiture
+{
+    private $_marque;
+    private $_modele;
+
+    public function __construct($marque, $modele)
+    {
+        $this->_marque = $marque;
+        $this->_modele = $modele;
     }
-    $v1 = new Voiture("Peugeot","408");
-    echo $v1->getInfos();
+    public function getMarque(){
+        return $this->_marque;
+}
+    public function getModele(){
+    return $this->_modele;
+}
+    public function getInfos()
+    {
+        return $this->getMarque() . " " . $this->getModele();
+    }
+}
+$v1 = new Voiture("Peugeot", "408") ;
+echo $v1->getInfos()."<br>";
+
+class VoitureElec extends Voiture
+{
+    private $_autonomie;
+
+    public function __construct($marque, $modele, $autonomie)
+    {
+        parent::__construct($marque, $modele);
+        $this->_autonomie = $autonomie;
+    }
+    public function getInfos()
+    {
+        return parent::getInfos()." ".$this->_autonomie;
+    }
+
+    /**
+     * Set the value of _modele
+     *
+     * @return  self
+     */ 
+    public function set_modele($_modele)
+    {
+        $this->_modele = $_modele;
+
+        return $this;
+    }
+}
+$ve1 = new VoitureElec("BMW", "I3", "100");
+echo $ve1->getInfos();
