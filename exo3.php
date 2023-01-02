3483ef3f355cc3cb1ab7ff492e9336a9038641c9
Faustine67 <102908333+Faustine67@users.noreply.github.com>
2023-01-02 17:14:49 +0100

    Update exo3.php

--- exo3.php
+++ exo3.php
@@ -7,12 +7,3 @@
 echo '<a href="Elan Formation"> https://elan-formation.eu/ </a> ';
 
 ?>
-
-<!-- // <?php
- //$target = 'source.ext'; // This is the file that already exists
- //$link = 'newfile.ext'; // This the filename that you want to link it to
-
- //link($target, $link);
-
-// -->
-?>
